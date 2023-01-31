bfe82056fb68b308c1a4ee76a1aaa5dca5ee97a2
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-31 10:42:06 -0300

    Printed board game in screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
+
+import static application.UI.printBoard;
 
 public class Main {
     public static void main(String[] args) {
 
+        ChessMatch chessMatch = new ChessMatch();
+        printBoard(chessMatch.getPieces());
     }
 }
