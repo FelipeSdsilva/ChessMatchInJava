828a4a7ce2ae3d3a35eb5b8070de8b61e8036fa5
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-31 08:43:15 -0300

    Create two methods piece and piecePositon in class Board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,4 +27,12 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piecePosition(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
