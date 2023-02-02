68666186bfffeb6bdc0362e3912e6d5933d35727
Felipe Sousa <felipe.fps09@hotmail.com>
2023-02-02 18:49:28 -0300

    Methods initilized possibleMovies, performChessMove, replacePromotedPiece, Methods ok setupAllPiecesForMatch, placeNewPieceWithChessCoordinates and initialSetup

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import chess.enums.Color;
+import chess.pieces.*;
 
 public class ChessMatch {
 
@@ -15,6 +16,7 @@ public class ChessMatch {
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -25,5 +27,68 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void placeNewPieceWithChessCoordinates(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    public void initialSetup() {
+        setupAllPiecesForMatch();
+
+    }
+
+    public void possibleMovies() {
+
+    }
+
+    public void performChessMove() {
+    }
+
+    public void replacePromotedPiece(String type) {
+    }
+
+    public void setupAllPiecesForMatch() {
+        for (int i = 1; i <= board.getRows(); i++) {
+            for (int c = 0; c < board.getColumns(); c++) {
+                char j = (char) (97 + c);
+                if (i == 8 && j == 97 || i == 8 && j == 104) {
+                    placeNewPieceWithChessCoordinates(j, i, new Rook(board, Color.BLACK));
+                }
+                if (i == 8 && j == 98 || i == 8 && j == 103) {
+                    placeNewPieceWithChessCoordinates(j, i, new Knight(board, Color.BLACK));
+                }
+                if (i == 8 && j == 99 || i == 8 && j == 102) {
+                    placeNewPieceWithChessCoordinates(j, i, new Bishop(board, Color.BLACK));
+                }
+                if (i == 8 && j == 100) {
+                    placeNewPieceWithChessCoordinates(j, i, new King(board, Color.BLACK));
+                }
+                if (i == 8 && j == 101) {
+                    placeNewPieceWithChessCoordinates(j, i, new Queen(board, Color.BLACK));
+                }
+                if (i == 7 && j < 105) {
+                    placeNewPieceWithChessCoordinates(j, i, new Pawn(board, Color.BLACK));
+                }
+                if (i == 1 && j == 97 || i == 1 && j == 104) {
+                    placeNewPieceWithChessCoordinates(j, i, new Rook(board, Color.WHITE));
+                }
+                if (i == 1 && j == 98 || i == 1 && j == 103) {
+                    placeNewPieceWithChessCoordinates(j, i, new Knight(board, Color.WHITE));
+                }
+                if (i == 1 && j == 99 || i == 1 && j == 102) {
+                    placeNewPieceWithChessCoordinates(j, i, new Bishop(board, Color.WHITE));
+                }
+                if (i == 1 && j == 101) {
+                    placeNewPieceWithChessCoordinates(j, i, new King(board, Color.WHITE));
+                }
+                if (i == 1 && j == 100) {
+                    placeNewPieceWithChessCoordinates(j, i, new Queen(board, Color.WHITE));
+                }
+                if (i == 2 && j < 105) {
+                    placeNewPieceWithChessCoordinates(j, i, new Pawn(board, Color.WHITE));
+                }
+            }
+        }
     }
 }
