36fbb14d2c32d23be4f9ee6b102fa4513712d2bc
Felipe Sousa <felipe.fps09@hotmail.com>
2023-02-07 15:52:23 -0300

    impementation possibleMovies and isThereAnyPossibleMovie

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
     protected Position position;
     private Board board;
@@ -11,5 +11,23 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMovies();
+
+    public boolean possibleMovie(Position position) {
+        return possibleMovies()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMovie() {
+        boolean[][] mat = possibleMovies();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
