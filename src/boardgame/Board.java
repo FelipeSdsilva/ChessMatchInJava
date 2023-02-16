21fd014e1bfb5887878b97aab65a0aca1385be87
Felipe Sousa <felipe.fps09@hotmail.com>
2023-02-16 11:16:28 -0300

    Clear code application

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -11,9 +11,7 @@ public class Board {
     private final Piece[][] pieces;
 
     public Board(int rows, int columns) {
-        if (rows < 1 && columns < 1) {
-            throw new BoarderException(valueTheBoardInvalid());
-        }
+        if (rows < 1 && columns < 1) throw new BoarderException(valueTheBoardInvalid());
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -28,34 +26,25 @@ public class Board {
     }
 
     public Piece piece(int row, int column) {
-        if (!positionExists(row, column)) {
-            throw new BoarderException(notPositionOnBoard());
-        }
+        if (!positionExists(row, column)) throw new BoarderException(notPositionOnBoard());
         return pieces[row][column];
     }
 
     public Piece piecePosition(Position position) {
-        if (!positionExists(position)) {
-            throw new BoarderException(notPositionOnBoard());
-        }
+        if (!positionExists(position)) throw new BoarderException(notPositionOnBoard());
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
-        if (thereIsAPiece(position)) {
-            throw new BoarderException(existPieceInTheBoard() + position);
-        }
+        if (thereIsAPiece(position)) throw new BoarderException(existPieceInTheBoard() + position);
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
     public Piece removePiece(Position position) {
-        if (!positionExists(position)) {
-            throw new BoarderException(notPositionOnBoard());
-        }
-        if (piecePosition(position) == null) {
-            return null;
-        }
+        if (!positionExists(position)) throw new BoarderException(notPositionOnBoard());
+        if (piecePosition(position) == null) return null;
+
         Piece aux = piecePosition(position);
         aux.position = null;
         pieces[position.getRow()][position.getColumn()] = null;
@@ -72,9 +61,7 @@ public class Board {
     }
 
     public boolean thereIsAPiece(Position position) {
-        if (!positionExists(position)) {
-            throw new BoarderException(notPositionOnBoard());
-        }
+        if (!positionExists(position)) throw new BoarderException(notPositionOnBoard());
         return piecePosition(position) != null;
     }
 }
