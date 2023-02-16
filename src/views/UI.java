2097429a112ec20af0dae8d8e537011d69bbf66c
Felipe Sousa <felipe.fps09@hotmail.com>
2023-02-16 11:35:02 -0300

    Create method printBackgroundBoard, create logical for modification of color current player, clear class

--- src/views/UI.java
+++ src/views/UI.java
@@ -13,25 +13,11 @@ import java.util.Scanner;
 import static views.MessageError.chessValuesValid;
 
 public class UI {
-
-
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
     public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
     public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     public static void clearScreen() {
@@ -48,7 +34,6 @@ public class UI {
         } catch (RuntimeException e) {
             throw new InputMismatchException(chessValuesValid());
         }
-
     }
 
     public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
@@ -57,7 +42,9 @@ public class UI {
         System.out.println("\n" +
                 "Turn: " + chessMatch.getTurn()
                 + "\n" +
-                "Waiting player: " + chessMatch.getCurrentPlayer());
+                "Waiting player: " +
+                ((chessMatch.getCurrentPlayer() == Color.WHITE) ? ANSI_CYAN : ANSI_RED) + chessMatch.getCurrentPlayer()
+                + ANSI_RESET);
         if (chessMatch.getCheck()) {
             System.out.println("CHECK!");
         }
@@ -65,38 +52,42 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
+            System.out.print((i == 8) ? ANSI_WHITE_BACKGROUND : ANSI_BLACK_BACKGROUND + (8 - i) + " ");
+
             for (int j = 0; j < pieces.length; j++) {
+                printBackgroundBoard(i, j);
                 printPiece(pieces[i][j], false);
             }
-            System.out.println();
+            System.out.println(ANSI_RESET);
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println(ANSI_BLACK_BACKGROUND + "  a b c d e f g h " + ANSI_RESET);
     }
 
     public static void printBackgroundColorWithPossibleMovesOfPiece(ChessPiece[][] pieces, boolean[][] possibleMoves) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
+            System.out.print((i == 8) ? ANSI_WHITE_BACKGROUND : ANSI_BLACK_BACKGROUND + (8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
+                printBackgroundBoard(i, j);
                 printPiece(pieces[i][j], possibleMoves[i][j]);
             }
-            System.out.println();
+            System.out.println(ANSI_RESET);
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println(ANSI_BLACK_BACKGROUND + "  a b c d e f g h");
+    }
+
+    private static void printBackgroundBoard(int i, int j) {
+        if (i % 2 == 0 && j % 2 == 0) System.out.print(ANSI_WHITE_BACKGROUND);
+        if (i % 2 == 1 || j % 2 == 1) System.out.print(ANSI_BLACK_BACKGROUND);
+        if (j % 2 == 1 && i % 2 == 1) System.out.print(ANSI_WHITE_BACKGROUND);
     }
 
     public static void printPiece(ChessPiece piece, boolean background) {
-        if (background) {
-            System.out.print(ANSI_BLUE_BACKGROUND);
-        }
-        if (piece == null) {
-            System.out.print("- " + ANSI_RESET);
-        } else {
-            if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            } else {
-                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
-            }
+        if (background) System.out.print(ANSI_BLUE_BACKGROUND);
+
+        if (piece == null) System.out.print("  ");
+        else {
+            if (piece.getColor() == Color.WHITE) System.out.print(ANSI_CYAN + piece + ANSI_RESET);
+            else System.out.print(ANSI_RED + piece + ANSI_RESET);
         }
         System.out.print("");
     }
@@ -104,10 +95,22 @@ public class UI {
     private static void printCapturedPieces(List<ChessPiece> captured) {
         List<ChessPiece> captWithe = captured.stream().filter(x -> x.getColor() == Color.WHITE).toList();
         List<ChessPiece> captBlack = captured.stream().filter(x -> x.getColor() == Color.BLACK).toList();
-        System.out.println("Captured Pieces:" +
-                "\n" +
-                "White: " + ANSI_WHITE + Arrays.toString(captWithe.toArray()) + ANSI_RESET +
-                "\n" +
-                "Black: " + ANSI_YELLOW + Arrays.toString(captBlack.toArray()) + ANSI_RESET);
+        System.out.println("""
+                                
+                Captured Pieces:
+                                
+                White: """ + ANSI_CYAN + Arrays.toString(captWithe.toArray()) + ANSI_RESET +
+                "\nBlack: " + ANSI_RED + Arrays.toString(captBlack.toArray()) + ANSI_RESET);
     }
 }
+//public static final String ANSI_GREEN = "\
+//public static final String ANSI_BLACK = "\u001B[30m";u001B[32m";
+//public static final String ANSI_YELLOW = "\u001B[33m";
+//public static final String ANSI_BLUE = "\u001B[34m";
+//public static final String ANSI_PURPLE = "\u001B[35m";
+//public static final String ANSI_WHITE = "\u001B[37m";
+//public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+//public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+//public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+//public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+//public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
