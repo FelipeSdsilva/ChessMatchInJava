d79cf81768de32eac33823badc3e0ce7fd763c74
Felipe Sousa <felipe.fps09@hotmail.com>
2023-02-19 17:43:27 -0300

    Class ChessPosition clear and use class soming register position in match

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -5,24 +5,9 @@ import exceptions.ChessException;
 
 import static views.MessageError.chessValuesValid;
 
-public class ChessPosition {
-    private char column;
-    private int row;
-
-    public ChessPosition(char column, int row) {
-        if (column < 'a' && column > 'h' || row < 1 && row > 8) {
-            throw new ChessException(chessValuesValid());
-        }
-        this.column = column;
-        this.row = row;
-    }
-
-    public char getColumn() {
-        return column;
-    }
-
-    public int getRow() {
-        return row;
+public record ChessPosition(char column, int row) {
+    public ChessPosition {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) throw new ChessException(chessValuesValid());
     }
 
     protected Position toPosition() {
@@ -34,7 +19,5 @@ public class ChessPosition {
     }
 
     @Override
-    public String toString() {
-        return "" + column + row;
-    }
+    public String toString() { return "" + column + row; }
 }
